fba1c1beac907c22d836597a4e7b6399350fa5f4
WJX2001 <1758807220@qq.com>
2024-11-02 13:57:15 +0800

    feat: chainlink 获取随机数

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,38 +10,51 @@
 
 pragma solidity ^0.8.7;
 
-error Raffle__NotEnoughETHEntered ();
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract Raffle {
-    
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2Plus {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    uint256 public s_subscriptionId;
 
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 subscriptionId
+    ) VRFConsumerBaseV2Plus(0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B) {
         i_entranceFee = entranceFee;
+        s_subscriptionId = subscriptionId;
     }
 
     function enterRaffle() public payable {
-      if(msg.value < i_entranceFee) {
-        revert Raffle__NotEnoughETHEntered();
-      }
-      s_players.push(payable(msg.sender));
-      // Emit an event when we update a dynamic array or mapping
-      // Named events with the function name reversed
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
 
-      emit RaffleEnter(msg.sender);
+        emit RaffleEnter(msg.sender);
     }
 
-    function pickRandomWinner() external {
-      // request the random number
-      // once we get it, do something with it
+    function requestRandomWinner() external {
+        // request the random number
+        // once we get it, do something with it
+        // 2 transaction process
     }
 
+    // 填充随机数
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
 
     /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
